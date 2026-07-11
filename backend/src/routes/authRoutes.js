3a584d00003d2eb7891e438bf7f258475b0dc2a2
Artur Mendonça Arruda <arturmendoncaarruda@gmail.com>
2026-07-11 18:42:56 -0300

    fix(backend): corrige extracao do token jwt do header authorization

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -2,6 +2,7 @@ export default async function (fastify, opts) {
   
   fastify.post('/authorize', { onRequest: [fastify.authenticate] }, async (req, reply) => {
     const { query_type, target_id } = req.body;
+    const token = req.headers.authorization.split(' ')[1];
 
     const authDecision = await fastify.grpcClient.AuthorizeQuery({
       token_jwt: token,
